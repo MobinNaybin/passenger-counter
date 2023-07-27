80be672f3183247e042dabe6322f8bc791cef431
MobinNaybin <128417509+MobinNaybin@users.noreply.github.com>
2023-07-27 18:39:04 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let sumEl = document.getElementById('sum-el')
 let count = 0
+let sum = 0
 
 function increment() {
     count += 1
@@ -10,6 +12,17 @@ function increment() {
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
+    sum += count
+    sumEl.textContent = 'Sum: ' + sum
     countEl.textContent = 0
     count = 0
+    
+}
+
+function clean() {
+    saveEl.textContent = 'Previous entries: ';
+    sumEl.textContent = 'Sum: '
+    countEl.textContent = 0
+    count = 0
+    sum = 0
 }
